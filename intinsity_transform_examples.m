5ec5458f0ad08749fa8f7c5a232f901678d93477
mtnasir <70468370+mtnasir@users.noreply.github.com>
2025-11-03 07:52:13 +0300

    m2

--- intinsity_transform_examples.m
+++ intinsity_transform_examples.m
@@ -43,6 +43,6 @@ plot((0:255)/255, T);             % Plot input→output intensity mapping in [0,
 
 %% adaptive hist
 I = imread('tire.tif');
-J = adapthisteq(I,'clipLimit',0.02,'Distribution','rayleigh');
-imshowpair(I,J,'montage');
-title('Original Image (left) and Contrast Enhanced Image (right)')
+J = adapthisteq(I,'clipLimit',0.02,'Distribution','rayleigh'); % Contrast Limited AHE
+imshowpair(I,J,'montage'); % Side-by-side comparison
+title('Original Image (left) and Contrast Enhanced Image (right)') 
